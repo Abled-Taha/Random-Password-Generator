9ad142f010fb16ea57059926ce5a6cb4b4cc1a8f
Abled <abledtaha@gmail.com>
2021-11-06 15:27:25 +0500

    Added Database functionality for localhost

--- src/Functions.py
+++ src/Functions.py
@@ -2,124 +2,94 @@ import os
 import json
 import Encryptor
 import RandomPasswordGenerator as RPG
+import pymongo
 
-def validateSignIn(username, password):
-    if os.path.isfile(f"Files/Accounts/{username}.json"):
-        encryptedPassword = Encryptor.Encrypt(password)
-        os.chdir("Files/Accounts")
-        with open(f"{username}.json") as File:
-            data = json.load(File)
-        os.chdir("../..")
+url = "localhost"
+port = 27017
+client = pymongo.MongoClient(url, port)
+db = client.RandomPasswordGenerator
+users = db.users
+credentials = db.credentials
+
+def validateSignIn(email, password):
+    try:
+        user = users.find_one({"email":email})
+        dbpassword = user["password"]
+    except:
+        return False
+    if password == dbpassword:
+        return True
     else:
         return False
-    if data["MainPassword"] == encryptedPassword:
-           return True
-    else:
-        return False
 
-def validateSignUp(username, password, confirmPassword):
+def validateSignUp(email, password, confirmPassword):
     if password == confirmPassword:
         pass
-    if username != "":
+    if email != "":
+        pass
+    if email.__contains__('@'):
         pass
     else:
         return False
-    allDone = createAccount(username, password)
+    allDone = createAccount(email, password)
 
     return allDone
 
-def createAccount(username, password):
-    if os.path.isdir("Files/Accounts"):
-        if os.path.isfile(f"Files/Accounts/{username}.json"):
-            return False
+def createAccount(email, password):
+    user = users.find_one({"email":email})
+    if user == None:
+        db.users.insert_one({"email":email, "password":password})
+        return True
     else:
-        os.mkdir("Files/Accounts")
+        return False
 
-    encryptedPassword = Encryptor.Encrypt(password)
-    os.chdir("Files/Accounts")
-    dict = {"MainPassword" : f"{encryptedPassword}"}
-    jsonObj = json.dumps(dict, indent=1)
-    with open(f"{username}.json", "a+") as File:
-        File.write(jsonObj)
-    os.chdir("../..")
-    return True
-
-def Details(username):
+def Details(email):
     # Returning Key Count
-    os.chdir("Files/Accounts")
-    with open(f"{username}.json") as File:
-        data = json.load(File)
-    keyCount = len(data.keys())
-    keyCount -= 1
-    os.chdir("../..")
+    usercredentials = credentials.find_one({"email":email})
+    passwords = usercredentials["passwords"]
+    keyCount = len(passwords.keys())
 
     # Returning Password Name
-    os.chdir("Files/Accounts")
-    with open(f"{username}.json") as File:
-        data = json.load(File)
     keyList = []
-    for key, value in data.items():
+    for key, value in passwords.items():
         keyList.append(key)
-    keyList.pop(0)
-    os.chdir("../..")
 
     return keyCount, keyList
 
-def getPass(objectName, username):
-    os.chdir("Files/Accounts")
-    with open(f"{username}.json") as File:
-        data = json.load(File)
-    EncryptedPassword = data[f"{objectName}"]
-    DecryptedPassword = Encryptor.Decrypt(EncryptedPassword)
-    os.chdir("../..")
+def getPass(objectName, email):
+    usercredentials = credentials.find_one({"email":email})
+    passwords = usercredentials["passwords"]
+    password = passwords[f"{objectName}"]
 
-    return DecryptedPassword
+    return password
 
-def createPassword(Name, Characters, Capital, Small, Numbers, Symbols, username):
-    Password = RPG.generateNewPassword(Characters, Capital, Small, Numbers, Symbols)
-    EncryptedPassword = Encryptor.Encrypt(Password)
+def createPassword(Name, Characters, Capital, Small, Numbers, Symbols, email):
+    usercredentials = credentials.find_one({"email":email})
+    passwords = usercredentials["passwords"]
+    password = RPG.generateNewPassword(Characters, Capital, Small, Numbers, Symbols)
     os.chdir("Files/Accounts")
 
-    with open(f"{username}.json") as File:
-        data = json.load(File)
-    data[f"{Name}"] = EncryptedPassword
-    os.remove(f"{username}.json")
+    passwords[f"{Name}"] = password
+    db.credentials.update_one({"_id":usercredentials["_id"]}, {"$set":{"passwords":passwords}})
 
-    with open(f"{username}.json", "a+") as File:
-        json.dump(data, File)
-    os.chdir("../..")
-
-def deletePassword(Name, username):
-    os.chdir("Files/Accounts")
-
-    with open(f"{username}.json") as File:
-        data = json.load(File)
+def deletePassword(Name, email):
+    usercredentials = credentials.find_one({"email":email})
+    passwords = usercredentials["passwords"]
     try:
-        del data[f"{Name}"]
+        del passwords[f"{Name}"]
+        db.credentials.update_one({"_id":usercredentials["_id"]}, {"$set":{"passwords":passwords}})
     except:
-        os.chdir("../..")
         return False
-    os.remove(f"{username}.json")
-
-    with open(f"{username}.json", "a+") as File:
-        json.dump(data, File)
-    os.chdir("../..")
-
     return True
 
-def deleteUser(username, password):
-    if os.path.isfile(f"Files/Accounts/{username}.json"):
-        encryptedPassword = Encryptor.Encrypt(password)
-        os.chdir("Files/Accounts")
-        with open(f"{username}.json") as File:
-            data = json.load(File)
-        os.chdir("../..")
-    else:
+def deleteUser(email, password):
+    try:
+        user = users.find_one({"email":email})
+        dbpassword = user["password"]
+    except:
         return False
-    if data["MainPassword"] == encryptedPassword:
-        os.chdir("Files/Accounts")
-        os.remove(f"{username}.json")
-        os.chdir("../..")
+    if password == dbpassword:
+        db.users.delete_one({"_id":user["_id"]})
         return True
     else:
         return False
